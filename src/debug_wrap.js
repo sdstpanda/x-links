1a3819d2494ce4640e944f7236e30d817520f5a7
dnsev-h <dnsev-h@users.noreply.github.com>
2015-10-11 01:14:20 -0400

    Improve debugging script
    Add function call counting as an additional performance check
    Helps to make sure functions aren't being called when they shouldn't be
    The list of function names is approximate and may not be perfect

--- src/debug_wrap.js
+++ src/debug_wrap.js
@@ -8,7 +8,10 @@
 	// begin_debug
 
 	var wrap_setup = function () {
-		var node = null;
+		var error_node = null;
+		var function_names = [];
+		var total_counter = 0;
+		var function_counters = {};
 		var format_stack = function (stack) {
 			var output = "",
 				line, i, ii;
@@ -22,7 +25,7 @@
 			return output;
 		};
 		var log = function (exception) {
-			if (node === null) {
+			if (error_node === null) {
 				var n0 = document.body || document.documentElement,
 					n1 = document.createElement("div"),
 					n2 = document.createElement("textarea");
@@ -35,20 +38,28 @@
 				n1.appendChild(n2);
 				if (n0) n0.appendChild(n1);
 
-				node = n2;
+				error_node = n2;
 			}
 
 			var s = "";
-			if (node.value.length > 0) s += "\n====================\n";
+			if (error_node.value.length > 0) s += "\n====================\n";
 			s += "" + exception + "\n" + (format_stack("" + exception.stack));
-			node.value += s;
+			error_node.value += s;
 
 			console.log("Exception:", exception);
 		};
 		try {
-			Function.prototype._w = function () {
+			Function.prototype._w = function (fn_index) {
 				var fn = this;
 				return function () {
+					++total_counter;
+					if (fn_index in function_counters) {
+						++function_counters[fn_index];
+					}
+					else {
+						function_counters[fn_index] = 1;
+					}
+
 					try {
 						return fn.apply(this, arguments);
 					}
@@ -60,6 +71,40 @@
 			};
 		}
 		catch (e) {}
+
+		// Performance checking loop
+		var check_interval_index = 0;
+		setInterval(function () {
+			++check_interval_index;
+			if (total_counter === 0) return;
+
+			var keys = Object.keys(function_counters),
+				sortable = [],
+				i;
+
+			for (i = 0; i < keys.length; ++i) {
+				sortable.push([ function_counters[keys[i]], parseInt(keys[i], 10) ]);
+			}
+
+			sortable.sort(function (a, b) {
+				if (a[0] > b[0]) return -1;
+				if (a[0] < b[0]) return 1;
+				if (a[1] > b[1]) return -1;
+				if (a[1] < b[1]) return 1;
+				return 0;
+			});
+
+			for (i = 0; i < sortable.length; ++i) {
+				sortable[i] = function_names[sortable[i][1]] + ": " + sortable[i][0];
+			}
+
+			console.log("========================================");
+			console.log("Debug Function Call Counter:", "Init + " + check_interval_index + "s: " + total_counter + " calls");
+			console.log("Debug Function Call Counter:", sortable);
+			console.log("========================================");
+			total_counter = 0;
+			function_counters = {};
+		}, 1000);
 	};
 
 	var stringify_function = function (fn, indent) {
@@ -71,9 +116,12 @@
 			parens = 0,
 			start_parens = -1,
 			token_pre = null,
+			token_pre_name = null,
 			function_starts = [],
 			output = "",
-			tokens, token, before, after, indent, c, i;
+			function_names = [],
+			function_name_pos = -1,
+			tokens, token, before, after, indent, name, fs, t, c, i;
 
 		ComplexionJS(instance);
 		tokens = instance.tokenize(source);
@@ -84,10 +132,14 @@
 			after = "";
 			c = token.content;
 
-			if (!token.isAnyType([ "WHITESPACE", "LINE_TERMINATOR", "SINGLE_LINE_COMMENT", "MULTI_LINE_COMMENT", "BOM", "SHEBANG", "UNKNOWN", "IMPLICIT_SEMICOLON" ])) {
+			if (!token.isAnyType([ "WHITESPACE", "LINE_TERMINATOR", "SINGLE_LINE_COMMENT", "MULTI_LINE_COMMENT", "BOM", "SHEBANG", "UNKNOWN" ])) {
 				if (token.isType("KEYWORD")) {
 					if (c === "function") {
-						function_starts.push(parens);
+						name = (token_pre_name === null ? "?" : token_pre_name.content);
+						t = (function_starts.length > 0 ? function_names[function_starts[function_starts.length - 1][1]] : "?");
+						if (t !== "?") name = t + "." + name;
+						function_starts.push([ parens, function_names.length ]);
+						function_names.push(name);
 					}
 				}
 				else if (token.isType("PUNCTUATOR")) {
@@ -96,14 +148,31 @@
 					}
 					else if (c === "]" || c === ")" || c === "}") {
 						--parens;
-						if (c === "}" && function_starts.length > 0 && function_starts[function_starts.length - 1] === parens) {
+						if (
+							c === "}" &&
+							function_starts.length > 0 &&
+							(fs = function_starts[function_starts.length - 1])[0] === parens
+						) {
 							function_starts.pop();
 							if (start_parens >= 0 && parens >= start_parens) {
-								after = "._w()";
+								after = "._w(" + fs[1] + ")";
 							}
 						}
 					}
 				}
+
+				if (token.isAnyType([ "KEYWORD", "NULL_LITERAL", "NUMERIC_LITERAL", "IDENTIFIER_NAME" ])) {
+					token_pre_name = token;
+				}
+				else if (token.isAnyType([ "PUNCTUATOR" ])) {
+					if (c !== "[" && c !== "(" && c !== "{" && c !== ":" && c !== "=") {
+						token_pre_name = null;
+					}
+				}
+				else {
+					token_pre_name = null;
+				}
+
 				token_pre = token;
 			}
 			else if (start_parens < 0 && token.isType("SINGLE_LINE_COMMENT")) {
@@ -112,6 +181,12 @@
 					indent = /(?:^|\n)([\t ]*)$/.exec(output);
 					indent = (indent === null) ? "" : indent[1];
 					c = "(" + stringify_function(wrap_setup, indent) + ")();";
+
+					// Position
+					function_name_pos = c.indexOf("var function_names = [];");
+					if (function_name_pos >= 0) {
+						function_name_pos += output.length + before.length + ("var function_names = ").length;
+					}
 				}
 			}
 
@@ -119,6 +194,10 @@
 			output += before;
 			output += c;
 			output += after;
+		}
+
+		if (function_name_pos >= 0) {
+			output = output.substr(0, function_name_pos) + JSON.stringify(function_names) + output.substr(function_name_pos + 2);
 		}
 
 		return output;
