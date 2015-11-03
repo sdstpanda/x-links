2b81238450c78bd85ff99a84dbd8cbe65fe43dea
dnsev-h <dnsev-h@users.noreply.github.com>
2015-11-03 17:52:25 -0500

    X-links Release 1.2.0

---
+++ builds/x-links.meta.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        X-links
+// @namespace   dnsev-h
+// @author      dnsev-h
+// @version     1.2.0
+// @description Making your browsing experience on 4chan and friends more pleasurable
+// @include     http://boards.4chan.org/*
+// @include     https://boards.4chan.org/*
+// @include     http://8ch.net/*
+// @include     https://8ch.net/*
+// @include     http://desustorage.org/*
+// @include     https://desustorage.org/*
+// @include     http://fgts.jp/*
+// @include     https://fgts.jp/*
+// @include     http://boards.38chan.net/*
+// @include     http://forums.e-hentai.org/*
+// @include     https://forums.e-hentai.org/*
+// @homepage    https://dnsev-h.github.io/x-links/
+// @supportURL  https://github.com/dnsev-h/x-links/issues
+// @updateURL   https://raw.githubusercontent.com/dnsev-h/x-links/stable/builds/x-links.meta.js
+// @downloadURL https://raw.githubusercontent.com/dnsev-h/x-links/stable/builds/x-links.user.js
+// @icon        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAA6klEQVR4Ae2ZoQ7CQBBE+VIMBoHBYBAYDKLmDAKFwPBha/sJIGebMMld9hK2l9nkmaa5vpdcr6Kb+Gg0nzb6rzlMgALsPf8kEmDTDG5AAeMHvEAowEnbFQwZoAAvbU/HA/j7W6XtAgYJUACP8dJ2dxQQkKYwH48CsgbwmDKDCUSkcWTTd2y55gmQmGQBCuh/RPJ3rAAmbQeQN0ABAWk7A75fQau0bUHaAAUE9vrywUfA9y6ISf8/QAH9j0gvsXfsAImpllZAzoBm6Yo1IV0rx64rIH0A+bIGAyDdvr8VsLYA/WJSgEaj0axtvoTvtkB/WJNGAAAAAElFTkSuQmCC
+// @icon64      data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAMAAACdt4HsAAAAOVBMVEUBAADmje/mku/mqu/mie/mXu/mdu/mbO/mhO/mm+/mf+/mY+/me+8AAADmpe/maO/mce/ml+/moe+i3TygAAAAAXRSTlMAQObYZgAAAJ9JREFUeF7tl0kKxDAQxCbJrPvy/8eOCqYgmMYPqFjXRrqUc8gONs+joXdLDix/qsAEe9hC4AlVQPIZ0gOSv3ABSa18heSAI5I/MEMlG8dFUsARyRNU8gJeSbcjyEkK9B6PV5rB8glyArV8B08lWvkAOYF6Pslv8FyikgMC3ccj+QU3cGQtJwVaeX2TvJYspwX88VQBye10eYHxvwCDwQ+bGFfRy77HgQAAAABJRU5ErkJggg==
+// @grant       GM_xmlhttpRequest
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_deleteValue
+// @grant       GM_listValues
+// @run-at      document-start
+// ==/UserScript==
