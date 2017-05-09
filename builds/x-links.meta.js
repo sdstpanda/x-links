c2d31e58d6a42206b831b9f49a319c4b0e649778
dnsev-h <dnsev-h@users.noreply.github.com>
2017-05-08 21:51:43 -0400

    Release 1.2.8.20

--- builds/x-links.meta.js
+++ builds/x-links.meta.js
@@ -2,7 +2,7 @@
 // @name        X-links
 // @namespace   dnsev-h
 // @author      dnsev-h
-// @version     1.2.8.19
+// @version     1.2.8.20
 // @description Making your browsing experience on 4chan and friends more pleasurable
 // @include     http://boards.4chan.org/*
 // @include     https://boards.4chan.org/*
